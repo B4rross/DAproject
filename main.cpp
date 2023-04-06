a209814112e589c6324a194ea28f7bba6bd72b08
unknown <up202108706@up.pt>
2023-04-06 20:35:33 +0100

    edmond karp debugged again

--- main.cpp
+++ main.cpp
@@ -7,46 +7,77 @@ int main() {
     CP.read_files();
 
     int n;
-    do{
-        cout<<"T2.1 - Max number of trains between stations\n";
-        cout<<"T2.2 -\n";
-        cout<<"T2.3 -\n";
-        cout<<"T2.4 -\n";
-        cout<<"T3.1 -\n";
-        cout<<"T4.1 -\n";
-        cout<<"T4.2 -\n";
-        cout<<"Insert your option:\n";
-        cin>>n;
-        if(n==1){
-            cout<<"Insert station A\n";
-            string a;
-            cin>>a;
-            cout<<"Insert station B\n";
-            string b;
-            cin>>b;
-            CP.T2_1maxflow(a,b);
-        }
-        if(n==2){
-            CP.T2_2maxflowAllStations();
-        }
-        if(n==3){
-            CP.test();
-        }
-        if(n==4){
+    do {
+        std::cout << "T2.1 - Max number of trains between stations\n";
+        std::cout << "T2.2 - Stations that require the Max num of trains among all pairs of stations\n";
+        std::cout << "T2.3 -\n";
+        std::cout << "T2.4 -\n";
+        std::cout << "T3.1 -\n";
+        std::cout << "T4.1 -\n";
+        std::cout << "T4.2 -\n";
+        std::cout << "Insert your option:\n";
+        std::cin >> n;
 
-        }
-        if(n==5){
+        switch (n) {
+            case 1: {
+                std::cin.ignore(); // ignore newline character left in the input stream
+                std::string a, b;
+                std::cout << "Enter station A: ";
+                std::getline(std::cin, a);
 
-        }
-        if(n==6){
+                std::cout << "Enter station B: ";
+                std::getline(std::cin, b);
 
-        }
-        if(n==7){
+                if (a.empty() || b.empty()) {
+                    std::cerr << "Error: Station names cannot be empty." << std::endl;
+                    break;
+                }
 
+                // call function to calculate max flow between stations A and B
+                CP.T2_1maxflow(a, b);
+                break;
+            }
+
+            case 2: {
+                CP.T2_2maxflowAllStations();
+                break;
+            }
+
+            case 3: {
+                CP.test();
+                break;
+            }
+
+            case 4: {
+
+                break;
+            }
+
+            case 5: {
+
+                break;
+            }
+
+            case 6: {
+
+                break;
+            }
+
+            case 7: {
+
+                break;
+            }
+
+            case 8: {
+                std::cout << "Exiting program..." << std::endl;
+                break;
+            }
+
+            default: {
+                std::cerr << "Error: Invalid option selected." << std::endl;
+                break;
+            }
         }
-        if(n>8){
-            cout<<"Invalid option\n";
-        }
-    }
-    while(n!=8);
+    } while (n != 8);
+
 }
